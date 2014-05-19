2a4ee8beea1f5b6ffa42ee0816b68cdf66d7be83
Anthony Edwards Jr <credential.helper>
2014-05-19 00:50:02 -0400

    made changes to my solution

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -2,16 +2,27 @@
 
 
 # I worked on this challenge [by myself, with: ].
+# Anthony Edwards 
+# Kate La
 
 # 2. Pseudocode
 
+# Input:pass 5 arguements into our method essay_writer
+# Output: output should be and essay in a proper order
 
-# Input:
-# Output:
 # Steps:
+# first define a method essay_writer
+# that method will take 5 arguements (title, topic, date, thesis_statment, pronoun)
+# title arguement would be assigned title of essay
+# topic arguement would be assigned topic of essay
+# date arguement would be assigned date of essay
+# thesis_statment arguement would be assigned thesis_statment of essay
+# teacher argument would be assigned to teacher of class
+# Utilize string concatenation to string together the arguments and form an essay
 
 
 # 3. Initial Solution
+=begin
 title = "My favorite Movie"
 topic = "Aladdin"
 partner = "Jasmine"
@@ -27,18 +38,20 @@ def essay_writer(title, topic, date, thesis, teacher, partner, antagonist)
    
    puts "\n \t" + thesis + " " +  topic + " combines great music, and all the magical aspect you would expect from a Disney Movie. "  + topic + " and " + partner + " make the perfect couple. " + "You have " + antagonist + " as the perfect antagonist to " + topic + " protagonist."  
 end
-Abu = {
-    :power => "quick and nimble"
-    }
 
-  title = Abu[:power] 
-  puts title
 
 essay_writer(title, topic, date, thesis, teacher, partner, antagonist)
+=end
 
 # 4. Refactored Solution
 
-
+def essay_writer(title, topic, date, thesis, teacher, partner, antagonist)
+   puts "\n" + title
+   puts topic
+   puts date + " Teacher: " + teacher
+   
+   puts "\n \t" + thesis + " " +  topic + " combines great music, and all the magical aspect you would expect from a Disney Movie. "  + topic + " and " + partner + " make the perfect couple. " + "You have " + antagonist + " as the perfect antagonist to " + topic + " protagonist."  
+end
 
 
 
@@ -47,11 +60,7 @@ essay_writer(title, topic, date, thesis, teacher, partner, antagonist)
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+essay_writer("My favorite Movie", "Aladdin","May 13, 2014", "Should Aladdin be considered the best movie of all-time?", "Mr. Philips","Jasmine", "Jafar")
 
-
-
-
-
-# 5. Reflection 
 
 
