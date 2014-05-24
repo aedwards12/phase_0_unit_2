5bff06433b077f42afb1c9fb8edd0dc314e56077
Anthony Edwards Jr <credential.helper>
2014-05-24 16:44:43 -0400

    updated my_solution

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -5,9 +5,18 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: a credit card number of length 16
+# Output: true of false if the credit card is valid or not
 # Steps:
+=begin
+initialize the CreditCard with the paramenter given then run an ArgumentError to catch bad paramenter passed into into
+define the check_card method
+iterate through array if index is even push onto new array else at every odd index doube the value,
+if new  value is great than 9 break it up into 2 seperate digits 
+for example "12 = '1' '2' " then push those 2 new digits onto the array 
+iterate over the new array and add up all the values
+if the total divides by ten evenly return true else return false
+=end
 
 
 # 3. Initial Solution
@@ -16,40 +25,50 @@
 # of exactly 16 digits
 class CreditCard
 	def initialize(card)
-		puts card
-		if card > 16 || card < 16
-			raise ArgumentError, "Invalid card digit" 
-		else	
-			@card = card
-		end
+	@card = card
+ 	raise ArgumentError, "Card Invalid length" unless @card === 16
+
+	puts card.size.to_s + " is my card size"
+			
 	end
 
 	def check_card
-		p "inside checksum" 
+		#p "inside checksum" 
 		sum = 0
 		digit_array= []
 		new_array= []
-		p @cards
+		#p @cards
 		digit_array = @card.to_s.chars.map(&:to_i)
-		p digit_array = @card.to_s.chars.map(&:to_i)
-		digit_array.each{|d| 
-			if d.odd? == true
+		#p digit_array = @card.to_s.chars.map(&:to_i)
+		digit_array.each_with_index{|d, index| 
+			if index.odd? == true
+				#puts d.to_s + "odd digit"
 				d = d * 2
+				#puts d.to_s = " odd digit * 2 "
 				if d > 9 
-					d.to_s.chars.map(&:to_i)
-					new_array << d[0]
-					new_array << d[1]
+					#puts d.to_s + "new array d"
+
+					#p d.to_s.chars.map(&:to_i)
+					new_array << d.to_s.chars.map(&:to_i)[0]
+					#p d.to_s.chars.map(&:to_i)[0].to_s
+					new_array << d.to_s.chars.map(&:to_i)[1]
+					#p  d.to_s.chars.map(&:to_i)[1].to_s
+					
 				else
 					new_array << d
+					#p new_array.to_s + "new array"
+
 				end
 			else
 				new_array << d
 			end
 					
 		}
+		#puts new_array.to_s + "new array"
 		new_array.each { |d|
-			sum += 0
+			sum += d
 		}
+		puts sum.to_s + " sum"
 
 		if sum % 10 == 0 
 			return true
@@ -71,10 +90,24 @@ end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 #my_card = CreditCard.new(22222)
-#my_card2 = CreditCard.new(4408041234567893)
-#puts my_card2.check_card
+=begin
+
+x=4408041234567893
+puts x
+card = CreditCard.new(x)
+puts card.check_card
+=end
 
 
 
 
 # 5. Reflection 
+=begin
+
+I am having a difficult time with this challenge. I wrote my own code. I got each task to pass
+individually but not all together. When i get the ArgumentError task to pass the return true or false tasks
+fail. When I get the true or false tasks to pass the ArgumentError tasks fail. I feel fairly confident with the 
+Learning Competencies of this challenge. I need to refactor this im sure there is an easier way to do this. For 
+reason my code keep returning 8 when i pass a 16 digit credit card number into it. 
+
+=end
