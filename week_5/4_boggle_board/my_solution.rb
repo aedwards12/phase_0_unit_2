ba82538f6dc0ed468ff7c55442c9846bbff1fe21
Anthony Edwards Jr <credential.helper>
2014-05-25 18:50:25 -0400

    updated boggle challenge

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -28,18 +28,33 @@ boggle_board = [["b", "r", "a", "e"],
 # Part 2: Write a method that takes a row number and returns all the elements in the row.  
 
 # Pseudocode
+=begin
+write a method that takes in a boggle_board game and row number
+iterate over array and push the value to array arr
+return the string of characters in the row. 
+=end
 
 
 # Initial Solution
 
-def elementsOfRow(row)
-	puts boggle_board[row][0] 
-end 
-# Refactored Solution
+def elementsOfRow(board, row)
+	arr = []
+	for i in 0..3
+	 arr << board[row][i]
+	end
 
+	str =  "The elements of row " + row.to_s +  " are " + arr.join(", ") + "."
+	
+end 
+
+
+# Refactored Solution
+# does not need refactoring
 
 # DRIVER TESTS GO BELOW THIS LINE
-
+y = elementsOfRow(boggle_board, 2)
+puts y 
+puts y.eql?  "The elements of row 2 are e, c, l, r."
 
 # Reflection 
 
@@ -49,15 +64,30 @@ end
 
 # Part 3: Now write a method that takes a column number and returns all the elements in the column.
 
-# Pseudocode
-
+# Pseudocode 
+=begin
+write a method that takes in a boggle_board game and column number
+iterate over array and push the value to array arr
+return the string of characters in the column. 
+=end
 # Initial Solution
+def elementsOfCol(board, col)
+	arr = []
+	for i in 0..3
+	 arr << board[i][col]
+	end
+	
+	str =  "The elements of column " + col.to_s +  " are " + arr.join(", ") + "."
 
+end 
 
 # Refactored Solution
 
-
+# does not need refactoring
 # DRIVER TESTS GO BELOW THIS LINE
 
+y =  elementsOfCol(boggle_board, 2)
+puts y
+puts y.eql? "The elements of column 2 are a, d, l, k."
 
 # Reflection 
