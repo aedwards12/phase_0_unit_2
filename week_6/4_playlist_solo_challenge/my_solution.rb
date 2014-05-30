de441b6876c6068d9c80face147567e63348672d
Anthony Edwards Jr <credential.helper>
2014-05-30 10:37:34 -0400

    updated my_solution

--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W6: Create a Playlist from Driver Code
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Pseudocode
@@ -10,15 +10,116 @@
 
 # Initial Solution
 
+=begin
+class Song
+	def initialize (song, artist)
+		@song = song
+		@artist = artist
+	end
 
+	def play
+		
+	end
+end
 
+class Playlist
+	attr_reader :my_playlist
+	@num_of_tracks = 0
+
+	def initialize (song1, song2, song3)
+		@my_playlist = [song1, song2, song3]
+		@num_of_tracks = 3
+	end	
+
+	def display
+
+	end
+
+	def play_all
+		p @my_playlist.each{|song, artist| 
+			puts @song 
+			puts @artist
+		}
+	end
+
+	def add (song1, song2)
+		@my_playlist << song1
+		@my_playlist << song2
+		@num_of_tracks += 2
+	end
+	def num_of_tracks
+		@num_of_tracks
+	end
+
+	def remove (song)
+		@my_playlist.delete(song)
+
+	end 
+
+	def includes?(song)
+		@my_playlist.include?(song)
+	end
+end
+=end
 
 # Refactored Solution
 
 
+class Song
+	attr_reader :song
+	attr_reader :artist
+	def initialize (song, artist)
+		@song = song
+		@artist = artist
+	end
 
+	def play
+		puts "Playing #{self.song}"
+	end
+end
 
+class Playlist
+	attr_reader :my_playlist
+	num_of_tracks = 0
 
+	def initialize (song1, song2, song3)
+		@my_playlist = [song1, song2, song3]
+		@num_of_tracks = 3
+	end	
+
+	def display
+		p "Your playlist includes"
+		p @my_playlist.each{|song| 
+			puts  "  - Song: " + song.song+ "  Artist: "  + song.artist
+		}
+	end
+
+	def play_all
+		p "Playing all songs"
+		
+	end
+
+	def add (song1, song2)
+		#song.inject(0) { |number| @my_playlist << song}
+		@my_playlist << song1
+		@my_playlist << song2
+		@num_of_tracks += 2
+	end
+	def num_of_tracks
+		p "You have " + @num_of_tracks.to_s + " track in this playlist."
+	end
+
+	def remove (song)
+		@my_playlist.delete(song)
+
+		p "Deleteing " + song.song + " from playlist."
+
+	end 
+
+	def includes?(song)
+		@my_playlist.include?(song)
+	end
+end
 
 # DRIVER TESTS GO BELOW THIS LINE
 one_by_one = Song.new("One by One", "Sirenia")
